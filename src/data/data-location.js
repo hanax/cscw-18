77ee2526664592c1847c77adc7cba0c0715550ce
Sanjay Kairam <sanjay.kairam@gmail.com>
2017-06-02 08:38:39 -0700

    Added first draft of location information

--- src/data/data-location.js
+++ src/data/data-location.js
@@ -1,3 +1,21 @@
 const locationIntro = {
-  content: marked("Us trying really hard to sell Jersey City")
+  content: `
+
+CSCW 2018 will take place at the **Hyatt Regency Jersey City on the Hudson** located in Jersey City, New Jersey. With its waterfront location, the venue has easy access to attractions, dining, and entertainment in Jersey City and nearby New York City.
+
+Jersey City has a rich history, acting as the first destination for many immigrants entering the U.S. through nearby Ellis Island, which operated from 1892 until 1954 and processed more than 12 million immigrant steamship passengers. Today its waterfront downtown reflects this multicultural influence through its vast array of food options and art. 
+
+Beyond the many sites to see in Jersey City itself (including Liberty State Park, the Statue of Liberty and Ellis Island, and Central Railroad Terminal of New Jersey) right across the Hudson river lies the New York City borough of Manhattan. Manhattan is NYC’s most densely populated borough, as well as its economic and administrative center and the city's historical birthplace. Three of world's 10 most-visited tourist attractions can be found in Manhattan (Times Square, Central Park, and Grand Central Terminal) along with many world-renowned bridges, such as the Brooklyn Bridge, skyscrapers (such as the Empire State Building), and parts (such as Central Park). 
+
+In addition to the sights in Manhattan, NYC’s boroughs also have plenty of their own. Brooklyn is the home of the iconic Brooklyn Bridge, Prospect Park, and Coney Island. Queens, home of the Queens Botanical Garden, the Jamaica Bay Wildlife Refuge, and MoMA's contemporary wing, PS1.  For baseball fans, the Bronx, home of the New York Yankees, is well worth the trip.  Last, but not least, the Staten Island ferry will take you not only from Manhattan to Staten Island, but will also offer some lovely views along the way. 
+
+## Access to NYC
+
+The Hyatt Regency is a short walk to the Exchange Place PATH Station with [frequent trains](http://www.panynj.gov/path/maps-schedules.html) running directly across the Hudson to the World Trade Center site.
+
+With 2,000 bridges and tunnels in the Greater NYC area and one of the busiest public transit systems in the world, getting around the area can be daunting… so we have made a map to help you find some spots to visit.
+
+## Local Attractions
+
+`
 };
