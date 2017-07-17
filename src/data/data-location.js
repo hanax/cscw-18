be2e2718d142132962943f2447d1af4c18df674b
Sanjay Kairam <skairam@justin.tv>
2017-07-17 10:58:49 -0700

    Added links for hotel

--- src/data/data-location.js
+++ src/data/data-location.js
@@ -2,7 +2,7 @@ const locationIntro = {
   content : marked(
 
 `
-CSCW 2018 will take place at the **Hyatt Regency Jersey City on the Hudson** located in Jersey City, New Jersey, minutes away from Manhattan via train or ferry. With its waterfront location, the venue has easy access to attractions, dining, and entertainment in New York City.
+CSCW 2018 will take place at the **[Hyatt Regency Jersey City on the Hudson](https://jerseycity.regency.hyatt.com/)** located in Jersey City, New Jersey, minutes away from Manhattan via train or ferry. With its waterfront location, the venue has easy access to attractions, dining, and entertainment in New York City.
 
 Lovingly nicknamed the Big Apple, New York is home to some of the most driven, creative, adaptive, and passionate people in the world. There is always something going on and always somewhere to go! New York City is conveniently surrounded by 3 international airports (Newark is recommended for this venue) and connected by multiple train, ferry, and subway systems. It’s no wonder that despite being the most densely populated city in the US, residents and visitors frequently mention feeling connected to the city and the neighborhoods they explore!
 
@@ -14,7 +14,7 @@ As Frank Sinatra sang, “If you can make in New York, you can make it anywhere.
 
 ## Access to NYC
 
-The Hyatt Regency is a short walk to the **Exchange Place PATH Station** with [frequent trains](http://www.panynj.gov/path/maps-schedules.html) running directly across the Hudson to the World Trade Center site. The PATH also runs further north in Manhattan which makes connecting to the MTA subway trains like the 4,5,6 and A, C, E quite effortless. The nearby ferry terminal, Paulus Hook, offers rides to Wall Street, the World Financial Center, and Midtown (W 39th). From the Hyatt Regency, taking public transportation, you can get to the Financial District in less than 15 minutes, or to Times Square in about 30 minutes. Getting to the Whitney Museum and the New Museum also takes about 30 minutes, and the Empire State Building is 35 minutes away. You can get to the Metropolitan Museum of Art on the Upper East side and other Museum Mile attractions in less than an hour, again, all on public transport.
+The [Hyatt Regency](https://jerseycity.regency.hyatt.com/) is a short walk to the **Exchange Place PATH Station** with [frequent trains](http://www.panynj.gov/path/maps-schedules.html) running directly across the Hudson to the World Trade Center site. The PATH also runs further north in Manhattan which makes connecting to the MTA subway trains like the 4,5,6 and A, C, E quite effortless. The nearby ferry terminal, Paulus Hook, offers rides to Wall Street, the World Financial Center, and Midtown (W 39th). From the Hyatt Regency, taking public transportation, you can get to the Financial District in less than 15 minutes, or to Times Square in about 30 minutes. Getting to the Whitney Museum and the New Museum also takes about 30 minutes, and the Empire State Building is 35 minutes away. You can get to the Metropolitan Museum of Art on the Upper East side and other Museum Mile attractions in less than an hour, again, all on public transport.
 
 With 2,000 bridges and tunnels in the Greater NYC area and one of the largest public transit systems in the world, getting around the area is a breeze, and to make it even easier, we have made a map to help you find some spots to visit!
 
