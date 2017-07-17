9909e8811095d1348cfdb694fddcca1aaae49023
Sanjay Kairam <sanjay.kairam@gmail.com>
2017-07-17 08:32:59 -0700

    Moved markdown out to separate JS

--- src/data/data-location.js
+++ src/data/data-location.js
@@ -1,8 +1,10 @@
 const locationIntro = {
-  title: "Unused",
-  content: marked(
+  items: [
+    {
+      title: "Location Overview",
+      content: `
 
-"CSCW 2018 will take place at the **Hyatt Regency Jersey City on the Hudson** located in Jersey City, New Jersey, minutes away from Manhattan via train or ferry. With its waterfront location, the venue has easy access to attractions, dining, and entertainment in New York City.
+CSCW 2018 will take place at the **Hyatt Regency Jersey City on the Hudson** located in Jersey City, New Jersey, minutes away from Manhattan via train or ferry. With its waterfront location, the venue has easy access to attractions, dining, and entertainment in New York City.
 
 Lovingly nicknamed the Big Apple, New York is home to some of the most driven, creative, adaptive, and passionate people in the world. There is always something going on and always somewhere to go! New York City is conveniently surrounded by 3 international airports (Newark is recommended for this venue) and connected by multiple train, ferry, and subway systems. It’s no wonder that despite being the most densely populated city in the US, residents and visitors frequently mention feeling connected to the city and the neighborhoods they explore!
 
@@ -20,6 +22,12 @@ With 2,000 bridges and tunnels in the Greater NYC area and one of the largest pu
 
 ## Local Attractions
 
-  "
-)
+`
+    }
+  ]
 };
+
+locationIntro.items = locationIntro.items.map((n) => {
+  n.content = marked(n.content);
+  return n;
+});
